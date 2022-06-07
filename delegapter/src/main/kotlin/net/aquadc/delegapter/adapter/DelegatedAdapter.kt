742956d0bc3e986d6459b4419a4faa8e29834b35
Miha-x64 <mihlo1996@rambler.ru>
2022-06-07 16:10:39 +0300

    add DelegatedAdapter

---
+++ delegapter/src/main/kotlin/net/aquadc/delegapter/adapter/DelegatedAdapter.kt
@@ -0,0 +1,29 @@
+package net.aquadc.delegapter.adapter
+
+import android.view.ViewGroup
+import net.aquadc.delegapter.MutableDelegapter
+import net.aquadc.delegapter.VH
+
+/**
+ * An adapter implementation with Delegapter inside.
+ */
+open class DelegatedAdapter @JvmOverloads constructor(
+    parent: MutableDelegapter? = null,
+    initialCapacity: Int = -1,
+) : VHAdapter<VH<*, *, *>>() {
+
+    val data = MutableDelegapter(this, parent, initialCapacity)
+
+    final override fun getItemCount(): Int =
+        data.size
+
+    final override fun getItemViewType(position: Int): Int =
+        data.viewTypeAt(position)
+
+    final override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH<*, *, *> =
+        data.createViewHolder(parent, viewType)
+
+    final override fun onBindViewHolder(holder: VH<*, *, *>, position: Int, payloads: List<Any>): Unit =
+        data.bindViewHolder(holder, position, payloads)
+
+}
