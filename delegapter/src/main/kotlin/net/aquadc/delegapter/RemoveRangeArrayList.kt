d1a29b2f344cf293f5712beae7bc0b88093a41e2
Miha-x64 <mihlo1996@rambler.ru>
2023-09-17 19:25:11 +0300

    move RR Lists to the correct folder

--- delegapter/src/main/kotlin/net/aquadc/delegapter/RemoveRangeArrayList.kt
+++ delegapter/src/main/kotlin/net/aquadc/delegapter/RemoveRangeArrayList.kt
@@ -1,17 +1,24 @@
 package net.aquadc.delegapter
 
+/**
+ * [MutableList] with public [removeRange] function.
+ * @author Mike Gorünóv
+ */
 interface RemoveRangeMutableList<T> : MutableList<T> {
+    /**
+     * @see [AbstractMutableList.removeRange]
+     */
     fun removeRange(fromIndex: Int, toIndex: Int)
 }
 
 /**
- * ArrayList with public removeRange().
+ * [ArrayList] with public [removeRange] function.
  * @author Mike Gorünóv
  */
 class RemoveRangeArrayList<E> : ArrayList<E>, RemoveRangeMutableList<E> {
     private constructor() : super()
     internal constructor(initialCapacity: Int) : super(initialCapacity)
-    internal constructor(copyFrom: Collection<E>?) : super(copyFrom)
+    internal constructor(copyFrom: Collection<E>) : super(copyFrom)
 
     override fun removeRange(fromIndex: Int, toIndex: Int) {
         super.removeRange(fromIndex, toIndex)
